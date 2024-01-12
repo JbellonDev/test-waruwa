31fa5eb141967777aed30ce56da252b7ebf39659
Noith <nmontealegre@imaginacolombia.com>
2024-01-11 19:20:15 -0500

    Fixed bug about header

--- components/Header.tsx
+++ components/Header.tsx
@@ -3,7 +3,7 @@ import LogoImage from "@/components/LogoImage";
 export default function Header({size}: { size?: 'sm' | undefined }) {
   return (
     <div style={{ height: 94 }}>
-      <div className="flex flex-col gap-4 items-center fixed top-0 left-0 w-full z-10 bg-white">
+      <div className="flex flex-col gap-4 items-center fixed top-0 left-0 w-full z-40 bg-white">
         <div className="flex gap-8 justify-center items-center pt-4">
           <LogoImage size={size}/>
         </div>
